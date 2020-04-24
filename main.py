5ecd32250613e9f7e76364bcb9f625104aa4b6d5
woolllff <utkarsh.agarwal@iiitb.org>
2020-04-24 22:31:43 +0530

    Driver programs added

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import numpy as np 
+import json
+import algo2 as algo3
+
+
+f= open("testCases/testcase.txt","r")
+
+f2 = open("testCases/falsetestcase.txt","w")
+
+contents = f.readlines()
+#print(contents)
+
+
+for i in range(int(len(contents)/4)):
+
+    n = json.loads(contents[i*4 + 0])
+    a = json.loads(contents[i*4 + 1])
+    b = json.loads(contents[i*4 + 2])
+    ans = json.loads(contents[i*4 + 3])
+
+    #print(type(n),type(a),type(b),type(ans))
+
+    ans2 = algo3.makebetterhalf(a,b,n)
+
+    if (ans != ans2):
+        print(n,'\n',a,'\n',b,'\n',ans,'\n',ans2)
+        l = [str(n) +"\n",str(a)+"\n",str(b)+"\n",str(ans)+"\n",str(ans2)+'\n']
+        for i in l:
+            f2.write(i)
+        c = []
+        for i in range(len(a)):
+            c.append(a[i]-b[i])
+        c.sort()
+        print(c)
+
+
+f.close()
+f2.close()
---
+++ makeTestCases.py
@@ -0,0 +1,25 @@
+import random
+import numpy as np 
+import json
+import algo1 as algo1
+
+
+f= open("testCases/testcase.txt","w+")
+f.close()
+
+f= open("testCases/testcase.txt","a")
+
+for i in range(100):
+
+    n = 20
+    a = list(np.random.randint(1000, size = 2*n))
+    b = list(np.random.randint(1000, size = 2*n))
+    ans = algo1.makeBetterHalf(a,b,n)
+    #l = [n,a,b,ans]
+    l = [str(n) +"\n",str(a)+"\n",str(b)+"\n",str(ans)+"\n"]
+    for i in l:
+        f.write(i)
+
+
+
+f.close()
